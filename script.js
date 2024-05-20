e10373b767bba9cff28180237562af4fe77e0ae4
satishrathod-dev <167071223+satishrathod-dev@users.noreply.github.com>
2024-05-20 15:52:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const arr = str.split("");
+	for(let i=0; i<arr.length; i++){
+		if(arr[i]==arr[i+1]){
+			continue;
+		}
+		else{
+			return arr[i];
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
